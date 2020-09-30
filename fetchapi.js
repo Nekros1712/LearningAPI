61db31bce45d3f5fc7fb4d978c68ee960e854ff0
Sravya-Sistla <67946777+Sravya-Sistla@users.noreply.github.com>
2020-09-30 17:17:08 +0530

    Update fetchapi.js

--- fetchapi.js
+++ fetchapi.js
@@ -4,3 +4,15 @@ async function getRandom() {
     let res = await response.json();
     document.getElementById('jokebox').innerHTML = '<h2><b>' + res.value + '</b></h2>';
 }
+
+async function getCategList()
+{
+  let results = '' ;
+  let url = 'https://api.chucknorris.io/jokes/categories';
+  let response = await fetch(url);
+  let res = await response.json();
+  for (ele of res )
+    results += ele + " ";
+  document.getElementById('jokebox').innerHTML ='<h2><b>' + results + '</b></h2>'
+  
+}
